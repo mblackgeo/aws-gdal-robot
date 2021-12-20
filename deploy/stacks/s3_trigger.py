68ddeec8728c0eeabecb11ed47706e1ad718691c
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-12-20 16:19:09 +0000

    Add initial CDK S3 trigger stack, still WIP

---
+++ deploy/stacks/s3_trigger.py
@@ -0,0 +1,28 @@
+from aws_cdk import Stack
+from aws_cdk import aws_lambda as lambda_
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_notifications
+from constructs import Construct
+
+
+class S3TriggerStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # create lambda function
+        function = lambda_.Function(
+            self,
+            f"{id}-lambda-batch-trigger",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            handler="handler.main",
+            code=lambda_.Code.from_asset("./lambda"),
+        )
+
+        # create s3 bucket
+        bucket = s3.Bucket(self, f"{id}-s3bucket")
+
+        # create s3 notification for lambda function
+        notification = aws_s3_notifications.LambdaDestination(function)
+
+        # assign notification for the s3 event type (ex: OBJECT_CREATED)
+        bucket.add_event_notification(s3.EventType.OBJECT_CREATED, notification)
