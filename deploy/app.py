789142719f83dff2217ef3dfa7e4d979dcba0166
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-12-21 15:27:42 +0000

    Import and use all stacks

--- deploy/app.py
+++ deploy/app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
+from stacks.batch import BatchStack
+from stacks.networking import NetworkingStack
 from stacks.s3_trigger import S3TriggerStack
 
 app = cdk.App()
-S3TriggerStack(app, "S3TriggerStack")
+
+networking = NetworkingStack(app, "NetworkingStack")
+s3_stack = S3TriggerStack(app, "S3TriggerStack")
+batch_stack = BatchStack(
+    app, "BatchStack", s3_stack=s3_stack, networking_stack=networking
+)
 
 app.synth()
