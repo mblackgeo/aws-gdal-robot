1ade08be2644188df71a5160caec7008ef0bc8b8
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-12-21 15:27:00 +0000

    Add networking stack with initial VPC config

---
+++ deploy/stacks/networking.py
@@ -0,0 +1,15 @@
+from aws_cdk import Stack
+from aws_cdk import aws_ec2 as ec2
+from constructs import Construct
+
+
+class NetworkingStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.vpc = ec2.Vpc(
+            self,
+            "vpc",
+            cidr="10.0.0.0/16",
+            nat_gateways=0,  # TODO check if this is needed
+        )
