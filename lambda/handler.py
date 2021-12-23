f644a40ac83d84fd1eed5bb2d444605fa4c4d354
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-12-23 14:04:15 +0000

    Add some more logging and remove the filename from job name, use a uuid instead

--- lambda/handler.py
+++ lambda/handler.py
@@ -2,28 +2,32 @@ import json
 import os
 import urllib.parse
 from datetime import datetime
+from uuid import uuid4
 
 import boto3
 
 
 def main(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    print(f"Context : {context}")
+    print(f"Event : {json.dumps(event, indent=2)}")
 
     # Get the object from the event and show its content type
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
 
-    print(f"Submitting batch job to convert : {bucket}{key}")
+    print(f"Submitting batch job to convert : {bucket}/{key}")
 
     job_defn = os.environ["BATCH_JOB_DEFINITION"]
     job_queue = os.environ["BATCH_JOB_QUEUE"]
+    job_name = f"convert-{int(datetime.utcnow().timestamp())}-{uuid4()}"
 
     print(f"Using batch job defn : {job_defn}")
     print(f"Using batch job queue : {job_queue}")
+    print(f"Submitting job : {job_name}")
 
     # submit the s3 bucket and key to the AWS Batch job queue
     response = boto3.client("batch").submit_job(
-        jobName=f"convert-{bucket}-{key}-{int(datetime.utcnow().timestamp())}",
+        jobName=job_name,
         jobDefinition=job_defn,
         jobQueue=job_queue,
         containerOverrides={
