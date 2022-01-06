1e52a5bcde35200e139c0dd7308cac7f194346b3
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-01-06 15:11:38 +0000

    Add key suffix filters to the event notifications

--- deploy/stacks/s3_trigger.py
+++ deploy/stacks/s3_trigger.py
@@ -2,7 +2,7 @@ from aws_cdk import Stack
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_lambda as lambda_
 from aws_cdk import aws_s3 as s3
-from aws_cdk import aws_s3_notifications
+from aws_cdk import aws_s3_notifications as s3n
 from aws_cdk import aws_ssm as ssm
 from constructs import Construct
 
@@ -11,24 +11,24 @@ class S3TriggerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Get the S3 bucket that was created in the Shared Stack
+        # Get the input S3 bucket that was created in the Shared Stack
         bucket = s3.Bucket.from_bucket_arn(
             scope=self,
-            id=f"{construct_id}-s3-bucket",
-            bucket_arn=ssm.StringParameter.value_for_string_parameter(self, "s3-bucket-arn"),
+            id=f"{construct_id}-s3-input-bucket",
+            bucket_arn=ssm.StringParameter.value_for_string_parameter(self, "s3-input-bucket-arn"),
+        )
+
+        # Get the output S3 bucket
+        out_bucket = s3.Bucket.from_bucket_arn(
+            scope=self,
+            id=f"{construct_id}-s3-output-bucket",
+            bucket_arn=ssm.StringParameter.value_for_string_parameter(self, "s3-output-bucket-arn"),
         )
 
         # Grab the AWS Batch parameters from session manager
         # These are needed by the lambda function to send the AWS Batch job
         job_defn_name = ssm.StringParameter.value_for_string_parameter(self, "batch-job-definition")
         job_queue_name = ssm.StringParameter.value_for_string_parameter(self, "batch-job-queue")
-
-        # Get the output S3 bucket
-        out_bucket = s3.Bucket.from_bucket_arn(
-            scope=self,
-            id=f"{construct_id}-s3-outpuyt-bucket",
-            bucket_arn=ssm.StringParameter.value_for_string_parameter(self, f"s3-outpuyt-bucket-arn"),
-        )
 
         # create lambda function that uses the python handler
         # this will trigger the AWS Batch job
@@ -54,7 +54,16 @@ class S3TriggerStack(Stack):
             )
         )
 
-        # Create the notification trigger on the S3 bucket that will
-        # trigger Lambda when an object is created
-        notification = aws_s3_notifications.LambdaDestination(self.function)
-        bucket.add_event_notification(s3.EventType.OBJECT_CREATED, notification)
+        # Create the notification trigger on the S3 bucket that will trigger
+        # Lambda when an object is created
+        # Trigger only on specific file suffixes for tiff images
+        # NOTE: this is not really a great approach for determining the file is
+        # actually a tif, but there is no lightweight solution shy of getting
+        # GDAL to try and open the file
+        suffixes = [".tif", ".TIF", ".tiff", ".TIFF"]
+        for suffix in suffixes:
+            bucket.add_event_notification(
+                s3.EventType.OBJECT_CREATED,
+                s3n.LambdaDestination(self.function),
+                s3.NotificationKeyFilter(suffix=suffix),
+            )
