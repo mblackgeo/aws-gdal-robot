31c3432e313472e0142b70f9e4a3ce37807d25e7
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-01-14 11:40:34 +0000

    Simplify adding email subscription

--- deploy/stacks/monitoring.py
+++ deploy/stacks/monitoring.py
@@ -2,6 +2,7 @@ from aws_cdk import Stack
 from aws_cdk import aws_events as events
 from aws_cdk import aws_events_targets as events_targets
 from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as subscriptions
 from aws_cdk import aws_ssm as ssm
 from constructs import Construct
 
@@ -23,6 +24,10 @@ class MonitoringStack(Stack):
             topic_name="s3-to-cog-failures",
         )
 
+        topic.add_subscription(
+            subscriptions.EmailSubscription("mblack@sparkgeo.com")
+        )  # TODO move email address to config
+
         # Create a pattern that will match AWS Batch failures
         job_defn_name = ssm.StringParameter.value_for_string_parameter(self, "batch-job-definition")
         pattern = events.EventPattern(
@@ -38,13 +43,4 @@ class MonitoringStack(Stack):
             description="",
             event_pattern=pattern,
             targets=[events_targets.SnsTopic(topic=topic)],
-        )
-
-        # Add an email subscription
-        sns.Subscription(
-            self,
-            f"{construct_id}-email-subscription",
-            endpoint="mblack@sparkgeo.com",  # TODO get this from config
-            protocol=sns.SubscriptionProtocol.EMAIL,
-            topic=topic,
         )
